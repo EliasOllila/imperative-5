49126e421c9a8c9038041096ea63f6bc71b289e0
eliasollila <elias.ollila@syk.fi>
2022-08-21 22:13:21 +0300

    initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int A=100;
-    int B=51;
-    if()
-    {
-        if()
-    }
-        System.out.println(1);
-    else
-        System.out.println(0);
-    }
-    if ()
+        int A = 100;
+        int B = 50;
+        if (A >= 100) {
+            if (B <= 50)
+                System.out.println(1);
+        } else
+            System.out.println(0);
 
+        if(A>=100)
+        {
+            if(B<=50)
+                System.out.println(1);
+        }
+        else if (B>=100)
+        {
+        if(A<=100)
+            System.out.println(1);
+        }
+        else
+            System.out.println(0);
+
+
+    }
 }
